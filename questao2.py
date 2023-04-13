a2c512673870df64d4faa6acefac21e086c01a21
Paulo André <pauloandrejanga@gmail.com>
2023-04-13 15:21:40 -0300

    Update and rename Questões.py to questao2.py

--- questao2.py
+++ questao2.py
@@ -1,15 +1,4 @@
-#1)
-'''INDICE = 13
-SOMA = 0
-K = 0
-
-while K < INDICE:
-    K = K + 1
-    SOMA = SOMA + K
-
-print(SOMA)'''
-
-#2)
+#2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
 lista = [0,1]
 
